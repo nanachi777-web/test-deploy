ec61cadacb476dafbc7239bfd4112a9cc89a06eb
Nana <Nana@gamil.com>
2026-04-29 14:07:53 -0400

    fix traffic weight to latest revision

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ resource "azurerm_container_app" "example" {
     external_enabled = true
     target_port     = 80
     traffic_weight {
+      latest_revision = true
       percentage = 100
     }
   }
