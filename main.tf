7b4662532777d1ab1fac5cddd1544b8e551c4417
Nana <Nana@gamil.com>
2026-04-29 14:20:10 -0400

    fix registry name to make it unique

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "mycontainerregistry"
+  name                = "myaprilcontainerfortest"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
   sku                 = "Basic"
